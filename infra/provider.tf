1151cf1be6afe7863dbba134840359b44315ef90
Don Athalage <dknathalage@gmail.com>
2023-10-19 20:55:39 +1100

    test

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,7 +1,7 @@
 provider "google" {
   project     = local.gcp_project
   region      = local.gcp_region
-  credentials = file(local.gcp_credentials)
+  credentials = local.gcp_credentials
 }
 
 terraform {
