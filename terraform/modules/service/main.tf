b47f6395d6337b7bded86908ea4e26cbf0813cb8
Don Athalage <dknathalage@gmail.com>
2023-10-24 21:11:17 +1100

    asfasdasf

--- terraform/modules/service/main.tf
+++ terraform/modules/service/main.tf
@@ -29,11 +29,12 @@ resource "google_cloudbuild_trigger" "build-trigger" {
     }
     step {
       # use helm to reploy
-      name = "gcr.io/cloud-builders/helm"
+      name = "gcr.io/$PROJECT_ID/helm"
       dir  = "services/${var.name}"
       args = [
         "upgrade",
         "--install", "${var.name}", "../../../config/service",
+        "--values", "service.yaml",
         "--namespace", "${var.namespace_name}",
         "--set", "image.tag=$COMMIT_SHA"
       ]
