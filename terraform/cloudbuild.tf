a0ae72fb809d7f9dd79817bab4331c4ee4091e87
dknathalage <dknathalage@gmail.com>
2024-05-09 23:53:38 +1000

    updates

--- terraform/cloudbuild.tf
+++
@@ -1,5 +0,0 @@
-module "svc-front" {
-  source         = "./modules/service-cloudbuild"
-  name           = "front"
-  namespace_name = kubernetes_namespace.namespace1.metadata[0].name
-}
--- terraform/namespaces.tf
+++
@@ -1,7 +0,0 @@
-resource "kubernetes_namespace" "namespace1" {
-  metadata {
-    annotations = { "env" = "prod" }
-    labels      = { "env" = "prod" }
-    name        = "namespace1"
-  }
-}
